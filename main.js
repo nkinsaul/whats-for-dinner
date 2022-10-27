353193cbdbf711e6e5f18295c57d6e8a151ffdc0
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-10-27 15:38:34 -0700

    Update styling and begin work on main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+// When a user selects a dish option (don’t worry about “Entire Meal”) 
+// and then clicks the “Let’s Cook!” button, the user sees a random dish from 
+// the list of possible dishes for that category
+// When the dish name appears, the cookpot icon disappears
