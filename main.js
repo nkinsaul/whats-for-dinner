b787c30421c762de15b214c18ecf21c2dca9d809
Natalie Kinsaul <n.kinsaul@gmail.com>
2022-10-30 14:26:01 -0700

    Clean up code and adjust some styling.

--- main.js
+++ main.js
@@ -1,21 +1,66 @@
-// When a user selects a dish option (don’t worry about “Entire Meal”) 
-// and then clicks the “Let’s Cook!” button, the user sees a random dish from 
-// the list of possible dishes for that category
-// When the dish name appears, the cookpot icon disappears
 
-//will need to create arrays that will contain values for 
-//'side', 'main dish', and 'dessert'
-// will need to write a function that randomly selects an item from each of the arrays when
-// selected.
-
-// Will need to put event listeners on each of the radio buttons that corresponds to the appropriate 
-// array
-// Will need an event listener on the 'let's cook' button so when it is clicked, it will update
-// to display the test from the array
-
-var sides = ['Green Beans', 'Mashed Potatoes', 'French Fries', 'Salad', 'Butternut Squash', 'Mushroom Risotto'];
-var main_dishes = ['Portabello Mushroom Burger', 'Green Curry', 'Spaghetti', 'Carbanara', 'Chicken Parmesan', 'Rib-Eye Steak', 'Vegetable Stew'];
-var desserts = ['Strawberry Rhubarb Pie', 'Pecan Pie', 'Chocolate Cake', 'Tiramisu', 'Banana Bread', 'Kentucky Butter Cake'];
+var sides = [
+'Green Beans', 
+'Mashed Potatoes', 
+'French Fries', 
+'Salad', 
+'Butternut Squash', 
+'Mushroom Risotto', 
+'Miso Glazed Carrots', 
+'Coleslaw',
+'Garden Salad',
+'Crispy Potatoes',
+'Sweet Potato Tots',
+'Coconut Rice',
+'Caesar Salad',
+'Shrimp Summer Rolls',
+'Garlic Butter Mushrooms',
+'Hush Puppies'
+];
+var main_dishes = [
+'Portabello Mushroom Burger', 
+'Green Curry', 
+'Spaghetti and Meatballs', 
+'Carbanara', 
+'Chicken Parmesan', 
+'Rib-Eye Steak', 
+'Vegetable Stew',
+'Pineapple Chicken',
+'Shakshuka',
+'Thai Yellow Curry',
+'Bibimbap',
+'Butternut Squash Soup',
+'BBQ Chicken Burgers',
+'Ramen',
+'Empanadas',
+'Chicken Fried Rice',
+'Sheet Pan Fajitas',
+'Margherita Pizza'
+];
+var desserts = [
+'Strawberry Rhubarb Pie', 
+'Pecan Pie', 
+'Chocolate Cake', 
+'Tiramisu', 
+'Banana Bread',
+'Kentucky Butter Cake',
+'Apple Pie',
+'Lemon Meringue Pie',
+'Black Forest Cake',
+'Peach Cobbler',
+'Cheesecake',
+'Funfetti Cake',
+'Baklava',
+'Flan',
+'Macaroons',
+'Chocolate Cupcakes',
+'Pavlova',
+'Pumpkin Pie',
+'Key Lime Pie',
+'Tart Tatin',
+'Croissants',
+'Eclairs'
+];
 var selection = '';
 var dishSelection = ''
 var random_side = ''
@@ -72,68 +117,15 @@ function displayEntireMeal() {
 }
 
 function displayDish () {  
-    console.log(selection);
-    if (selection === '') {
-        return;
-    } else if (selection === 'ENTIRE-MEAL') {
-        foodImage.classList.add("hidden");
-        recipe_display.innerHTML = '';
-        displayEntireMeal();
-    } else if (selection === 'SIDE' || 'MAIN-DISH' || 'DESSERT') {
-        foodImage.classList.add("hidden");
-        recipe_display.innerHTML = '';
-        recipe_display.innerHTML += `
+  foodImage.classList.add("hidden");
+  recipe_display.innerHTML = '';
+  if (selection === 'ENTIRE-MEAL') {
+    updateSelection();
+    displayEntireMeal();
+  } else {
+    updateSelection();
+    recipe_display.innerHTML += `
         <h3>You should make:</h3>
-          <h2>${dishSelection}!</h2>`
-    } 
+            <h2>${dishSelection}!</h2>`
   }
-
-// function displayDish () {  
-//   foodImage.classList.add("hidden");
-//   recipe_display.innerHTML = '';
-//   if (selection === 'ENTIRE-MEAL') {
-//     displayEntireMeal();
-//   } else {
-//   recipe_display.innerHTML += `
-//     <h3>You should make:</h3>
-//         <h2>${dishSelection}!</h2>`
-//   }
-// }
-
-// need to write a conditional statement that checks to see if entire meal is selected, if it's not,
-// it will run as normal, but if entire meal IS selected, it will run the functionality I wrote
-// on the displayEntireMeal Function.
-
-// console.log(radio_buttons[1].value);
-
-// var side_radio_selector = document.querySelector("#side");
-// var main_dish_radio_selector = document.querySelector("#main-dish");
-// var dessert_radio_selector = document.querySelector("#dessert");
-
-// radio_buttons.addEventListener('click', function () {
-//     for (radio_buttons of radio_buttons) {
-//         console.log(event.target.value);
-//     }     
-// }
-
-// side_radio_selector.addEventListener();
-// main_dish_radio_selector.addEventListener();
-// dessert_radio_selector.addEventListener();
-
-// function recordInput () {
-    
-//     }
-// }
-
-// radio_buttons.addEventListener('click', selectButton)
-
-//   function selectButton () {
-//     for (radio_buttons of radio_buttons) {
-//         selection = event.target.value;
-//         console.log(event.target.value);
-//     }     
-// }
-
-// function logSelection() {
-//     console.log(selection);
-// }
+}
